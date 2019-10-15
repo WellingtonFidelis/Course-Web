491e955d48c97cd43065e4ddb057ce693907723c
WellingtonFidelis <wellingtonf20@gmail.com>
2019-10-15 16:29:38 -0300

    Passando do google drive para o git.

---
+++ Exercicios Limpos BootStrap/unidade09/_js/script.js
@@ -0,0 +1,7 @@
+$ (function () {
+    $ ('.carousel').carousel({
+        interval:4500,
+        //pause: "null" // caso eu queira que ao passar o mouse sob as imagens ela não parem de rolar
+        //wrap: false  // caso eu queira que as imagens parem de rolar na última.
+    });
+});
---
+++ Exercicios Limpos YouTube API v3/js/meuscript.js
@@ -0,0 +1,32 @@
+var nomeCanal = 'portadosfundos';
+var upload_id;
+
+$(document).ready(function() {
+    $.get("https://www.googleapis.com/youtube/v3/channels",{
+        part: 'contentDetails',
+        forUsername: nomeCanal,
+        key: 'AIzaSyBfF--i85Gtzs_U43aHk4sqz7R7HWqx7FI'},
+         function(data) {
+        upload_id = data.items[0].contentDetails.relatedPlaylists.uploads;
+        pegarVideos(upload_id);
+    }
+         )
+    function pegarVideos(id) {
+        $.get("https://www.googleapis.com/youtube/v3/playlistItems", {
+            part: 'snippet',
+            maxResults: 12,
+            playlistId: id,
+            key: 'AIzaSyBfF--i85Gtzs_U43aHk4sqz7R7HWqx7FI'},
+              function(data) {
+            var imagem;
+            var arquivo;
+            
+            $.each(data.items, function(i, item) {
+                imagem = item.snippet.thumbnails.medium.url;
+                arquivo = '<li><img src="' + imagem + '"/></li>';
+                $('div#janela ul').append(arquivo);
+            });
+        }
+             )
+    }
+});
