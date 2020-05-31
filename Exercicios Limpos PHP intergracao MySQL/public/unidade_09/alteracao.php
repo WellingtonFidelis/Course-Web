9615dfb9a2105845c2b0d3b93e471c70f4c80867
WellingtonFidelis <wellingtonf20@gmail.com>
2020-05-31 18:18:11 -0300

    Couse XII, Unit 09 - 407, select on DB with php.

--- Exercicios Limpos PHP intergracao MySQL/public/unidade_09/alteracao.php
+++ Exercicios Limpos PHP intergracao MySQL/public/unidade_09/alteracao.php
@@ -9,7 +9,7 @@
         $id = $_GET["codigo"];
         $transportadora .= "WHERE transportadoraID = {$id} ";
     } else {
-        $transportadora .= "WHERE transportadoraID = 1 ";
+        $transportadora .= "WHERE transportadoraID = 5 ";
     }
 
     $con_tranportadora = mysqli_query($conecta, $transportadora);
@@ -19,6 +19,15 @@
     }
 
     $info_transportadora = mysqli_fetch_assoc($con_tranportadora);
+
+    // consulta aos estados
+    $estados = "SELECT * ";
+    $estados .= "FROM estados "; 
+    $lista_estados = mysqli_query($conecta, $estados);
+
+    if (!$lista_estados) {
+        die("ERROR in DB.");    
+    }
 
 ?>
 
@@ -48,6 +57,29 @@
                     <input type="text" value="<?php echo utf8_encode($info_transportadora["cidade"]) ?>" name="cidade" id="cidade">
                     <label for="estados">Estados</label>
                     <select id="estados" name="estados">
+                        <?php 
+                           $meu_estado = $info_transportadora["estadoID"];
+                           while ($linha = mysqli_fetch_assoc($lista_estados)) {
+                               $estado_principal = $linha["estadoID"];
+                               if ($meu_estado == $estado_principal) {
+                        ?>
+                            <option value="<?php echo $linha[estadoID]; ?>" selected>
+                                <?php 
+                                    echo utf8_encode($linha["nome"]);
+                                ?>
+                            </option>
+                            <?php
+                               } else {
+                            ?>
+                            <option value="<?php echo $linha[estadoID]; ?>">
+                                <?php 
+                                    echo utf8_encode($linha["nome"]);
+                                ?>
+                            </option>
+                        <?php
+                               }
+                           }
+                        ?>
                     </select>
                     <label for="cep">CEP</label>
                     <input type="text" value="<?php echo utf8_encode($info_transportadora["cep"]) ?>" name="cep" id="cep">
