6afd5ee7bf32d38045a5bdf3f23f39749da054c2
WellingtonFidelis <wellingtonf20@gmail.com>
2020-07-08 20:58:05 -0300

    Couse XIII, Unit 04, 442 to 444 , load file XML with AJAX. .

--- Exercicios Limpos PHP com AJAX/unidade_04/exercicio.php
+++ Exercicios Limpos PHP com AJAX/unidade_04/exercicio.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
         <title>PHP com AJAX</title>
     </head>
 
@@ -10,7 +11,24 @@
         
         <script src="jquery.js"></script>
         <script>
+            $.ajax({
+                url:'_xml/produtos.xml'
+            }).then(successo, falha);
 
+            function successo (file) {
+               //console.log($(file).find('produto').find('nomeproduto').text());
+                var elemento;
+                elemento = "<ul>";
+                $(file).find('produto').each(function(){
+                    var nome = $(this).find('nomeproduto').text();
+                    elemento += "<li>" + nome + "</li>";
+                });
+                elemento += "</ul>";
+                $('div#listagem').html(elemento);
+
+            }
+
+            function falha () {}
         </script>
     </body>
 </html>
