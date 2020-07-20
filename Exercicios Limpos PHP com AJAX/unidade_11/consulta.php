8dda021278a403defbd5ad7f56f437b58f620b63
WellingtonFidelis <wellingtonf20@gmail.com>
2020-07-20 19:44:58 -0300

    Couse XIII, Unit 11, 463 to 466, JSON with callback and dynamic select and 2 select by first.

--- Exercicios Limpos PHP com AJAX/unidade_11/consulta.php
+++ Exercicios Limpos PHP com AJAX/unidade_11/consulta.php
@@ -13,6 +13,10 @@
                     <label for="categorias">Categorias</label>
                     <select id="categorias">
                     </select>
+                    <br>
+                    <label for="produtos">Produtos</label>
+                    <select id="produtos">
+                    </select>
                 </form>
             </div>
         </main>
@@ -20,6 +24,36 @@
         
         <script src="_js/jquery.js"></script>
         <script>
+            function retornarCategorias (data) {
+                //console.log(data);
+                var categorias = '';
+                $.each( data, function(key, value){
+                    //categorias += '<option value="' + value.catergoriaID + '">' + value.nomecategoria + '</option>';
+                    categorias += `<option value='${value.categoriaID}'>${value.nomecategoria}</option>`;
+                });
+                $('#categorias').html(categorias);
+            }
+            $('#categorias').change(function(e){
+                //console.log($(this).val());
+                var categoriaID = $(this).val();
+                $.ajax({
+                    type: 'GET',
+                    data: `categoriaID=${categoriaID}`,
+                    url: 'http://localhost/php_ajax/unidade_11/retornar_produtos.php',
+                    assync: false,
+                }).done(function (data) {
+                    //console.log($.parseJSON(data));
+                    //var list = '<ul>';
+                    var products = '';
+                    $.each($.parseJSON(data), function (key, value) {
+                        //list += `<li>${value.nomeproduto}</li>`;
+                        products += `<option value='${value.produtoID}'>${value.nomeproduto}</option>`;
+                    });
+                    //list += '</ul>';
+                    $('#produtos').html(products);
+                });
+            });
         </script>
+        <script src="http://localhost/php_ajax/unidade_11/retornar_categorias.php?callback=retornarCategorias"></script>
     </body>
 </html>
